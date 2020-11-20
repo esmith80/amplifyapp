82340ff9410f6b9b93c68d0a999a7faa27988a25
Edward Smith <edward.m.smith80@gmail.com>
2020-11-20 10:16:28 -0700

    add input fields

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from V2</h1>
+        <label for="fname">First name:</label>
+        <input type="text" id="fname" name="fname" />
+        <label for="lname">Last name:</label>
+        <input type="text" id="lname" name="lname" />
+        <input type="submit" value="Submit"></input>
         <button onclick={() => alert('hello world!')}>Click me</button>
       </header>
     </div>
