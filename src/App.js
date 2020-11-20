88891fcfa9a662d979c2fcfcc8e2ed1e42eba844
Edward Smith <edward.m.smith80@gmail.com>
2020-11-20 10:35:28 -0700

    change hello function alert and logo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function sayHello() {
+  alert("Hello from CARS!");
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://freesvg.org/img/combine-harvester-5.png" className="App-logo" alt="logo" />
         <h1>Hello from V2</h1>
         <label for="fname">First name:</label>
         <input type="text" id="fname" name="fname" />
         <label for="lname">Last name:</label>
         <input type="text" id="lname" name="lname" />
         <input type="submit" value="Submit"></input>
-        <button onclick={(e) => alert('hello world!')}>Click me</button>
+        <button onclick={sayHello()}>Say Hello</button>
       </header>
     </div>
   );
