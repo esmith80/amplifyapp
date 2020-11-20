6b74cb8581d36714bfc5e51dafbd22e1bd5cd42f
Edward Smith <edward.m.smith80@gmail.com>
2020-11-20 11:52:42 -0700

    change bground color and header

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src="https://freesvg.org/img/combine-harvester-5.png" className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>CARS</h1>
         <label for="fname">First name:</label>
         <input type="text" id="fname" name="fname" />
         <label for="lname">Last name:</label>
